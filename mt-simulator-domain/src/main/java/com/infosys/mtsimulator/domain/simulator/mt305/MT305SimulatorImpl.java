0bdbd2fddc87643b2213fe90b32f3f5da7b1f2bd
seagalputra <seagalputra@gmail.com>
2020-07-01 14:17:50 +0700

    Add MT305 simulator logic with Auto Match, Partial Match, and UnMatch

---
+++ mt-simulator-domain/src/main/java/com/infosys/mtsimulator/domain/simulator/mt305/MT305SimulatorImpl.java
@@ -0,0 +1,67 @@
+package com.infosys.mtsimulator.domain.simulator.mt305;
+
+import com.google.common.collect.Iterables;
+import com.infosys.mtsimulator.domain.simulator.SimulatorStrategy;
+import com.infosys.mtsimulator.domain.simulator.basesimulator.BaseSimulator;
+import javassist.NotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.infosys.mtsimulator.domain.constant.MTSimulatorConstant.*;
+
+@Component
+@AllArgsConstructor
+public class MT305SimulatorImpl implements SimulatorStrategy {
+
+    private final BaseSimulator baseSimulator;
+
+    @Override
+    public boolean isSupport(String simulatorType) {
+        return MT305.equals(simulatorType);
+    }
+
+    @Override
+    public String parse(String message, String type) throws NotFoundException {
+        String messageResult = baseSimulator.replaceApplicationHeader(message);
+        messageResult = baseSimulator.addPrefixCPTY(":20:", messageResult);
+        messageResult = baseSimulator.swapValue(":82A:", ":87A:", messageResult);
+
+        if (messageResult.contains(":23:BUY")) {
+            messageResult = messageResult.replace(":23:BUY", ":23:SELL");
+            messageResult = messageResult.replace(":34P:", ":34R:");
+            messageResult = baseSimulator.removeUnusedField(":22U:", messageResult);
+            messageResult = removeSenderCorrespondent(messageResult);
+        }
+
+        if (isPartialMatch(type) || isUnMatchType(type)) {
+            messageResult = baseSimulator.replaceValue(":30:", messageResult, ":30:20191225");
+        }
+
+        if (isUnMatchType(type))
+            messageResult = baseSimulator.replaceValue(":31E:", messageResult, ":31E:20200101");
+
+        return messageResult;
+    }
+
+    private String removeSenderCorrespondent(String message) {
+        List<String> listMessage = Arrays.asList(message.split("\n"));
+        int senderIndex = Iterables.indexOf(listMessage, element -> element.contains(":53A:"));
+        List<String> removedMessage = new ArrayList<>();
+
+        for (int index = 0; index < listMessage.size(); index++) {
+            if (isNotSenderCorrespondentIndex(senderIndex, index)) {
+                removedMessage.add(listMessage.get(index));
+            }
+        }
+
+        return String.join("\n", removedMessage);
+    }
+
+    private boolean isNotSenderCorrespondentIndex(int senderIndex, int currentIndex) {
+        return currentIndex != senderIndex && currentIndex != senderIndex + 1;
+    }
+}
---
+++ mt-simulator-domain/src/test/java/com/infosys/mtsimulator/domain/simulator/mt305/MT305SimulatorImplTest.java
@@ -0,0 +1,68 @@
+package com.infosys.mtsimulator.domain.simulator.mt305;
+
+import com.infosys.mtsimulator.domain.DomainConfiguration;
+import com.infosys.mtsimulator.domain.simulator.SimulatorStrategy;
+import javassist.NotFoundException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.infosys.mtsimulator.domain.constant.MTSimulatorConstant.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = DomainConfiguration.class)
+class MT305SimulatorImplTest {
+
+    @Qualifier("MT305SimulatorImpl")
+    @Autowired
+    private SimulatorStrategy mt305Simulator;
+    private static String buyMessage;
+    private static String sellMessage;
+
+    @BeforeAll
+    public static void setup() {
+        buyMessage = "{1:F01CENAIDJAAXXX0000000000}{2:I3051234556XXXXXN}{3:{108:4212}}{4:\n:15A:\n:20:4212-1\n:21:NEW\n:22:NEW/0014CENAJA\n:23:BUY/CALL/E/USD\n:82A:CENAIDJA\n:87A:1234556\n:30:200203\n:31G:200331/1500/JPTO\n:31E:200402\n:26F:PRINCIPAL\n:32B:USD2000000,\n:36:14000,\n:33B:IDR28000000000,\n:37K:PCT21,25\n:34P:200203USD424912,84\n:53A:/888888888\nCENAIDJA\n:57A:1234556\n:22U:FXVAOP\n:35B:\n:17Y:F\n:17Z:Y\n:22Q:FXO-FVTPL-OP1\n:17Q:Y\n-}";
+        sellMessage = "{1:F01CENAIDJAAXXX0000000000}{2:I3051234556XXXXXN}{3:{108:7261}}{4:\n:15A:\n:20:7261-1\n:21:NEW\n:22:NEW/0014CENAJA\n:23:SELL/PUT/E/USD\n:82A:CENAIDJA\n:87A:1234556\n:30:200203\n:31G:200330/1500/JPTO\n:31E:200402\n:26F:PRINCIPAL\n:32B:USD2000000,\n:36:14000,\n:33B:IDR28000000000,\n:37K:PCT0,\n:34R:200203USD92,35\n:57A:/888888888\nCENAIDJA\nFXVAOP\nF\nY\nFXO-FVTPL-OP1\nY\n-}";
+    }
+
+    @Test
+    public void shouldReturnParsedMessageWithBuyMessageInputInAutoMatch() {
+        String expectedBuyResult = "{1:F01CENAIDJAAXXX0000000000}{2:O3051234556XXXXXN}{3:{108:4212}}{4:\n:15A:\n:20:CPTY4212-1\n:21:NEW\n:22:NEW/0014CENAJA\n:23:SELL/CALL/E/USD\n:82A:1234556\n:87A:CENAIDJA\n:30:200203\n:31G:200331/1500/JPTO\n:31E:200402\n:26F:PRINCIPAL\n:32B:USD2000000,\n:36:14000,\n:33B:IDR28000000000,\n:37K:PCT21,25\n:34R:200203USD424912,84\n:57A:1234556\n-}";
+        String actualBuyResult = "";
+        try {
+            actualBuyResult = mt305Simulator.parse(buyMessage, AM);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(expectedBuyResult, actualBuyResult);
+    }
+
+    @Test
+    public void shouldReturnParsedMessageWithBuyMessageInputInPartialMatch() {
+        String expectedBuyResult = "{1:F01CENAIDJAAXXX0000000000}{2:O3051234556XXXXXN}{3:{108:4212}}{4:\n:15A:\n:20:CPTY4212-1\n:21:NEW\n:22:NEW/0014CENAJA\n:23:SELL/CALL/E/USD\n:82A:1234556\n:87A:CENAIDJA\n:30:20191225\n:31G:200331/1500/JPTO\n:31E:200402\n:26F:PRINCIPAL\n:32B:USD2000000,\n:36:14000,\n:33B:IDR28000000000,\n:37K:PCT21,25\n:34R:200203USD424912,84\n:57A:1234556\n-}";
+        String actualBuyResult = "";
+        try {
+            actualBuyResult = mt305Simulator.parse(buyMessage, PM);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(expectedBuyResult, actualBuyResult);
+    }
+
+    @Test
+    public void shouldReturnParsedMessageWithBuyMessageInputInUnMatch() {
+        String expectedBuyResult = "{1:F01CENAIDJAAXXX0000000000}{2:O3051234556XXXXXN}{3:{108:4212}}{4:\n:15A:\n:20:CPTY4212-1\n:21:NEW\n:22:NEW/0014CENAJA\n:23:SELL/CALL/E/USD\n:82A:1234556\n:87A:CENAIDJA\n:30:20191225\n:31G:200331/1500/JPTO\n:31E:20200101\n:26F:PRINCIPAL\n:32B:USD2000000,\n:36:14000,\n:33B:IDR28000000000,\n:37K:PCT21,25\n:34R:200203USD424912,84\n:57A:1234556\n-}";
+        String actualBuyResult = "";
+        try {
+            actualBuyResult = mt305Simulator.parse(buyMessage, UM);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(expectedBuyResult, actualBuyResult);
+    }
+}
