4cc8c433f1677d4e8d36b449abf0797017a528ee
seagalputra <seagalputra@gmail.com>
2020-07-02 15:05:02 +0700

    Load ftp configuration and display it into view

---
+++ mt-simulator-properties/src/main/java/com/infosys/mtsimulator/properties/ConfigProperties.java
@@ -0,0 +1,44 @@
+package com.infosys.mtsimulator.properties;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@PropertySource("classpath:ftpConfig.properties")
+@Data
+public class ConfigProperties {
+
+    private final Environment environment;
+
+    @Value("${Environment}")
+    private int numberOfEnvironment;
+
+    public ConfigProperties(Environment environment) {
+        this.environment = environment;
+    }
+
+    public List<Map<String, String>> getEnvironmentConfiguration() {
+        List<Map<String, String>> listEnvironment = new ArrayList<>();
+
+        for (int index = 1; index <= numberOfEnvironment; index++) {
+            Map<String, String> envProperty = new HashMap<>();
+            envProperty.put("name", environment.getProperty("env." + index + ".name"));
+            envProperty.put("ip", environment.getProperty("env." + index + ".ip"));
+            envProperty.put("path", environment.getProperty("env." + index + ".path"));
+            envProperty.put("user", environment.getProperty("env." + index + ".user"));
+            envProperty.put("pass", environment.getProperty("env." + index + ".pass"));
+
+            listEnvironment.add(envProperty);
+        }
+
+        return listEnvironment;
+    }
+}
--- mt-simulator-web/src/main/java/com/infosys/mtsimulator/web/controller/DashboardController.java
+++ mt-simulator-web/src/main/java/com/infosys/mtsimulator/web/controller/DashboardController.java
@@ -3,6 +3,7 @@ package com.infosys.mtsimulator.web.controller;
 import com.infosys.mtsimulator.domain.service.SimulatorService;
 import com.infosys.mtsimulator.api.request.ParseMessageRequest;
 import com.infosys.mtsimulator.api.response.ParseMessageResponse;
+import com.infosys.mtsimulator.properties.ConfigProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,25 +11,34 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @Slf4j
 public class DashboardController {
 
     private final SimulatorService simulatorService;
+    private final ConfigProperties configProperties;
+
     private ParseMessageResponse parseMessageResponse;
     private ParseMessageRequest parseMessageRequest;
 
-    public DashboardController(SimulatorService simulatorService) {
+    public DashboardController(SimulatorService simulatorService, ConfigProperties configProperties) {
         this.simulatorService = simulatorService;
+        this.configProperties = configProperties;
     }
 
     @GetMapping
     public ModelAndView messageForm() {
         ParseMessageRequest request = ParseMessageRequest.builder().build();
         ParseMessageResponse response = ParseMessageResponse.builder().build();
+        List<Map<String, String>> environmentProperties = configProperties.getEnvironmentConfiguration();
+
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("simulator", request);
         modelAndView.addObject("simulatorResult", response);
+        modelAndView.addObject("environmentConfiguration", environmentProperties);
         return modelAndView;
     }
 
